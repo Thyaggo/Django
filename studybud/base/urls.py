3f501802181b05b097c1d3cbffedc0de27ff9744
Thyaggo <santiagoandre.garciabedoya@ensea.fr>
2023-08-20 18:09:51 -0500

    Second Commit

---
+++ studybud/base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('rooms/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.delete, name="delete-room"),
+]
